983e83590122e5acf11f12c1429f971b32604a33
Mustafa Tunc <mtuncusa@gmail.com>
2022-06-25 10:35:27 -0500

    Basic Form completed without the final touch

--- supervisors1/src/App.js
+++ supervisors1/src/App.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import Form from './components/Form'
-import './App.css';
+import React from "react";
+import Form from "./components/Form";
+import classes from "./App.module.css";
 
 function App() {
-  return (<div>
-    <h1> Supervisors</h1>
-    <Form/>
+  return (
+    <div className={classes.app}>
+      <h1 className={classes.h1}> Supervisors</h1>
+      <Form />
     </div>
   );
 }
--- supervisors1/src/components/Form.js
+++ supervisors1/src/components/Form.js
@@ -1,29 +1,60 @@
+import classes from "./Form.module.css";
+
 function Form(props) {
   return (
-    <form>
-      {" "}
-      Notification Form
-      <div>
-        <label htmlFor="firstName">First Name</label>
-        <input type="text" placeholder="First Name" required id="firstName" />
-      </div>
-      <div>
-        <label htmlFor="lastName">Last Name</label>
-        <input type="text" placeholder="Last Name" required id="lastName" />
-      </div>
-      <div>
-        <label htmlFor="email">email</label>
-        <input type="email" placeholder="email" required id="email" />
-      </div>
-      <div>
-        <label htmlFor="phone">First Name</label>
-        <input type="Number" placeholder="Phone Number" required id="phone" />
-      </div>
-      <div>
-        <label htmlFor="supervisor">Supervisor</label>
-        <input type="select" placeholder="select" required id="supervisor" />
-      </div>
-    </form>
+    <div>
+      <form className={classes.form}>
+        <h2 className={classes.h2}>Notification Form</h2>
+
+        <div>
+          <div className={classes.control}>
+            <label className={classes.label} htmlFor="firstName">
+              First Name
+            </label>
+            <input type="text" required id="firstName" />
+          </div>
+          <div className={classes.control}>
+            <label type="select" className={classes.label} htmlFor="lastName">
+              Last Name
+            </label>
+            <input type="text" required id="lastName" />
+          </div>
+        </div>
+        <div className={classes.control}>
+          <label className={classes.label} htmlFor="email">
+            Email
+          </label>
+          <input type="email" required id="email" />
+        </div>
+        <div className={classes.control}>
+          <label className={classes.label} htmlFor="phone">
+            Phone Number
+          </label>
+          <input type="Number" required id="phone" />
+        </div>
+        <div className={classes.control}>
+          <label className={classes.label} htmlFor="supervisor">
+            Supervisor
+          </label>
+          <select
+            name="Supervisors"
+            placeholder="select"
+            required
+            id="supervisor"
+          >
+            <option value="select">Select...</option>
+            <option value="Supervisor1">Supervisor1</option>
+            <option value="Supervisor2">Supervisor2</option>
+            <option value="Supervisor3">Supervisor3</option>
+            <option value="Supervisor4">Supervisor4</option>
+            <option value="Supervisor5">Supervisor5</option>
+          </select>
+        </div>
+        <div className={classes.action}>
+          <button>Submit</button>
+        </div>
+      </form>
+    </div>
   );
 }
 
