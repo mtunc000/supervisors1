b65f0b8a1aad35f2bb7a96d5627dbd39d96fd7df
Mustafa Tunc <mtuncusa@gmail.com>
2022-06-27 00:52:11 -0500

    Validation and resetting logic has been added

--- supervisors1/src/components/Form.js
+++ supervisors1/src/components/Form.js
@@ -1,9 +1,33 @@
 import classes from "./Form.module.css";
 import Supervisors from "./Supervisors";
+import React, { useState } from "react";
 
 function Form(props) {
+  const [enteredFirstName, setEnteredFirstName] = useState("");
+  const [enteredLasttName, setEnteredLasttName] = useState("");
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredPhoneNumber, setEnteredPhoneNumber] = useState("");
+  const [enteredSupervisor, setEnteredSupervisor] = useState("");
   function submitHandler(event) {
     event.preventDefault();
+    if (
+      enteredFirstName.trim().length === 0 ||
+      enteredLasttName.trim().length === 0
+    ) {
+      return;
+    }
+    console.log(
+      enteredFirstName,
+      enteredLasttName,
+      enteredEmail,
+      enteredPhoneNumber,
+      enteredSupervisor
+    );
+    setEnteredFirstName("");
+    setEnteredLasttName("");
+    setEnteredEmail("");
+    setEnteredPhoneNumber("");
+    setEnteredSupervisor("");
 
     const supervisorData = {
       id: event.target.id.value,
@@ -16,6 +40,22 @@ function Form(props) {
     console.log(supervisorData);
     props.onAddSupervisor(supervisorData);
   }
+  const firstNameChangeHandler = (event) => {
+    setEnteredFirstName(event.target.value);
+  };
+
+  const lastNameChangeHandler = (event) => {
+    setEnteredLasttName(event.target.value);
+  };
+  const emailChangeHandler = (event) => {
+    setEnteredEmail(event.target.value);
+  };
+  const phoneNumberChangeHandler = (event) => {
+    setEnteredPhoneNumber(event.target.value);
+  };
+  //   const supervisorChangeHandler = (event) => {
+  //     setEnteredSupervisor(event.target.value);
+  //   };
 
   return (
     <div>
@@ -27,26 +67,48 @@ function Form(props) {
             <label className={classes.label} htmlFor="firstName">
               First Name
             </label>
-            <input type="text" required id="firstName" />
+            <input
+              type="text"
+              required
+              value={enteredFirstName}
+              onChange={firstNameChangeHandler}
+              id="firstName"
+            />
           </div>
           <div className={classes.control}>
             <label type="select" className={classes.label} htmlFor="lastName">
               Last Name
             </label>
-            <input type="text" required id="lastName" />
+            <input
+              type="text"
+              required
+              value={enteredLasttName}
+              onChange={lastNameChangeHandler}
+              id="lastName"
+            />
           </div>
         </div>
         <div className={classes.control}>
           <label className={classes.label} htmlFor="email">
             Email
           </label>
-          <input type="email" id="email" />
+          <input
+            type="email"
+            value={enteredEmail}
+            onChange={emailChangeHandler}
+            id="email"
+          />
         </div>
         <div className={classes.control}>
           <label className={classes.label} htmlFor="phone">
             Phone Number
           </label>
-          <input type="Number" id="phone" />
+          <input
+            type="Number"
+            value={enteredPhoneNumber}
+            onChange={phoneNumberChangeHandler}
+            id="phone"
+          />
         </div>
         <Supervisors />
 
